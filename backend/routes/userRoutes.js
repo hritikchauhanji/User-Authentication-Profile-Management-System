f4c7702229dd9247415983b605d8c73938c37fb0
Hritik Chauhan <hritikchji@gmail.com>
2025-10-07 15:43:22 +0530

    Add password validation to user login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import { User } from "../models/User.js";
 
 const router = express.Router();
 
+// Registration router
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -36,6 +37,42 @@ router.post("/register", async (req, res) => {
       _id: createdUser._id,
       name: createdUser.name,
       email: createdUser.email,
+      //   password: createdUser.password,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Login router
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // if email is required
+    if (!email) {
+      return res.status(400, "email is required");
+    }
+
+    // find user
+    const user = await User.findOne({ email });
+
+    // find user is not exist
+    if (!user) {
+      return res.status(404, "User is not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid user credentials" });
+    }
+
+    return res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: email.name,
+      profileImage: user.profileImage,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
