024fdf4b2a94f7b5580f6949983823c4aa4e1d26
Hritik Chauhan <hritikchji@gmail.com>
2025-10-07 13:28:15 +0530

    Set up Express server and configure listening port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 connectDB();
+
+const app = express();
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
