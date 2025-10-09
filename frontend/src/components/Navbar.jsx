9c3bbb8bd873cd869d1ef7673cf63a012915e78d
Hritik Chauhan <hritikchji@gmail.com>
2025-10-09 15:01:35 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,46 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
 import { isLoggedIn, removeToken } from "../utils/auth";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import api from "../api/axoisInstance";
 
 function Navbar() {
   const navigate = useNavigate();
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      if (isLoggedIn()) {
-        try {
-          const res = await api.get("/profile");
-          setUser(res.data);
-        } catch (error) {
-          toast.error("Error fetching user profile:", error);
-        }
-      }
-    };
-
-    fetchUser();
-  }, []);
-
   const logout = () => {
     removeToken();
-    toast.success("Logout successful!");
     navigate("/login");
   };
 
   return (
-    <nav className="text-white py-4 px-6 shadow flex justify-between items-center">
-      <div className="text-lg font-bold text-indigo-600">
-        {user ? user.name : "AuthApp"}
-      </div>
+    <nav className="text-indigo-600 py-4 px-6 shadow flex justify-between items-center">
+      <div className="text-lg font-bold">AuthApp</div>
       <div className="flex space-x-6 items-center">
+        <Link to="/" className="hidden md:block hover:text-gray-300">
+          Home
+        </Link>
         {isLoggedIn() ? (
           <>
-            <Link
-              to="/profile"
-              className="hover:text-indigo-700 text-indigo-600 font-semibold"
-            >
+            <Link to="/profile" className="hover:text-gray-300">
               Profile
             </Link>
             <button
@@ -52,15 +29,12 @@ function Navbar() {
           </>
         ) : (
           <>
-            <Link
-              to="/login"
-              className="hover:text-indigo-700 text-indigo-600 font-semibold"
-            >
+            <Link to="/login" className="hover:text-gray-300">
               Login
             </Link>
             <Link
               to="/register"
-              className="bg-indigo-600 hover:bg-indigo-700 px-4 py-2 rounded text-white font-medium transition"
+              className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded text-white font-medium transition"
             >
               Register
             </Link>
